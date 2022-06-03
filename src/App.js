dd073c1973dd0b6384cc4d0cdcc9093978da327f
Shaker Ahamed <ahamedshaker7@gmail.com>
2022-06-03 16:36:14 +0600

    Ready to go with react

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import { Route, Routes } from "react-router-dom";
+import Home  from "./Pages/Home/Home";
+import Contact  from "./Pages/Contact/Contact";
+import Login  from "./Pages/Login/Login";
+import Hotels  from "./Pages/Hotels/Hotels";
+import Hotel  from "./Pages/Hotel/Hotel";
+
+
+
 function App() {
   return (
     <div>
-      Hello world!
+        <Routes>
+          <Route path="/" element={<Home></Home>}></Route>
+          <Route path="/hotels" element={<Hotels></Hotels>}></Route>
+          <Route path="/hotels/:id" element={<Hotel></Hotel>}></Route>
+          <Route path="/contact" element={<Contact></Contact>}></Route>
+          <Route path="/login" element={<Login></Login>}></Route>
+        </Routes>
     </div>
   );
 }
---
+++ src/Pages/Hotels/Hotels.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Hotels = () => {
+    return (
+        <div>
+            hotel list
+        </div>
+    );
+};
+
+export default Hotels;
